9b0251c704ff38196cacebfe283f244933229f41
Andrewgaabriel <andrew.gabrielgomes@gmail.com>
2022-08-19 11:47:09 -0300

    resolve bugs insuportaveis

--- geradorafd.py
+++ geradorafd.py
@@ -1,64 +1,52 @@
-from importlib.resources import contents
 import sys
 
 
-
-
+""" -----------------Abrindo arquivo de log------------------------------------------------------- """
 def openFile(fileName):
-    """Recebe o nome do arquivo e retorna o arquivo
-    Args:
-        fileName (str): nome do arquivo
-    Returns:
-        file: arquivo aberto
-    """
-    
     try:
         file = open(fileName, 'r')
         return file
-    
-    except IOError:
-        print("Arquivo não encontrado")
-        sys.exit()
-        
-        
-        
+    except:
+        print('Erro ao abrir arquivo')
+""" -------------------------------------------------------------------------------------------- """
+
+
+
+
+""" -----------------------Função para imprimir conteúdo de um vetor/lista/arquivo--------------- """
 def printFile(file):
-    """Recebe o arquivo e imprime o conteúdo
-    Args:
-        file (file): arquivo aberto
-    """
-    
     for line in file:
         print(line)
 
+""" -------------------------------------------------------------------------------------------- """
 
-def getParameters():
-    """Recebe os parâmetros passados pelo usuário
-    Returns:
-        fileName (str): nome do arquivo
-    """
-    
-    if len(sys.argv) == 2:
-        fileName = sys.argv[1]
-        return fileName
-    else:
-        print("Uso: geradorafd.py <nomeDoArquivo>")
-        sys.exit()
-        
-        
-        
-def getFileContent(file):
-    """Receve o arquivo e retorna o conteúdo
-    Args:
-        file (file): arquivo aberto
-    Returns:
-        contents (vetor): conteúdo do arquivo
 
-    """
-    contents = []
+
+
+""" --------------------Função que pega o nome do arquivo de entrada------------------------------ """
+def getParam(arg):
+    return sys.argv[arg]
+
+""" -------------------------------------------------------------------------------------------- """
+
+
+
+
+""" ----------------Pega os dados do arquivo e coloca em um vetor-------------------------------- """
+def getData(file):
+    data = list()
     for line in file:
-        contents.append(line)
-    return contents
+        data.append(line)
+    return data
+""" -------------------------------------------------------------------------------------------- """
 
 
 
+
+file = openFile(getParam(1))                    # Abre o arquivo de entrada
+#printFile(file)                                # Imprime o conteúdo do arquivo
+allData = getData(file)                         # Pega os dados do arquivo e coloca em um vetor
+printFile(allData)                              # Imprime o conteúdo do vetor
+
+
+file.close()                                    # Fecha o arquivo de entrada
