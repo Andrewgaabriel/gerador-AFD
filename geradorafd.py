eae131e792a7c9eb0b237b93c21797590063ee82
Andrew <andrew.gabrielgomes@gmail.com>
2022-08-20 15:17:22 -0300

    Implementa todas as funções ( estão funcionando :) ) de parseamento dos dados da entrada.

--- geradorafd.py
+++ geradorafd.py
@@ -42,11 +42,111 @@ def getData(file):
 
 
 
+""" -------------------Função que pega os tokens sujos------------------ """
+def getTokens(data):
+    dirtyTokens = []
 
-file = openFile(getParam(1))                    # Abre o arquivo de entrada
-#printFile(file)                                # Imprime o conteúdo do arquivo
-allData = getData(file)                         # Pega os dados do arquivo e coloca em um vetor
+    for line in data:
+        if line == '\n':
+            break
+        else:
+            dirtyTokens.append(line)
+    
+    
+
+    return removeQuebraLinha(dirtyTokens)
+""" -------------------------------------------------------------------------------------------- """
+
+""" -------------------Função que pega os tokens sujos------------------ """
+def getGR(data):
+    data.reverse()
+    grs = []
+
+    for line in data:
+        if line == '\n':
+            break
+        else:
+            grs.append(line)
+    
+    
+
+    return removeQuebraLinha(grs)
+""" -------------------------------------------------------------------------------------------- """
+
+
+
+
+def removeQuebraLinha(data):
+    cleaned = []
+
+    for token in data:
+        cleaned.append(token.replace('\n', ''))
+
+    return cleaned
+
+
+
+def parseTokens(tokens):
+    parsed = []
+
+    for token in tokens:
+        for char in token:
+            parsed.append(char)
+    
+    #remove as duplicatas e ordena
+    parsed = sorted(set(parsed))
+    #parsed = list(set(parsed))
+
+    return parsed
+
+
+def parseGR(grs):
+    parsedGRs = []
+
+    
+    for gr in grs:
+
+        completedRule = []
+
+        dirtyRuleName = gr.split('::=')[0]
+        ruleName = dirtyRuleName.replace('<','').replace('>','')
+
+        dirtyRules = gr.split('::=')[1]
+        rules = dirtyRules.split('|')
+
+        completedRule.append(ruleName)
+        completedRule.append(rules)
+
+        parsedGRs.append(completedRule)
+
+    return parsedGRs
+
+
+
+
+
+
+print("TRATAMENTO DE ARQUIVO\n")
+allData = getData(openFile(getParam(1)))        # Pega os dados do arquivo e coloca em um vetor
 printFile(allData)                              # Imprime o conteúdo do vetor
+print("FIM DO TRATAMENTO DE ARQUIVO\n\n")
 
 
-file.close()                                    # Fecha o arquivo de entrada
+print("TRATAMENTO DE TOKENS\n")
+tokens = getTokens(allData)                     # Pega os tokens sujos
+printFile(tokens)                               # Imprime o conteúdo do vetor
+print("\nPARSEA OS TOKENS\n\n")
+parsedTokens = parseTokens(tokens)              # Parseia os tokens
+printFile(parsedTokens)                         # Imprime o conteúdo do vetor
+print("\nFIM DO TRATAMENTO DE TOKENS\n\n")
+
+
+
+print("TRATAMENTO DE GR\n")
+grs = getGR(allData)                            # Pega os GRs
+printFile(grs)                                  # Imprime o conteúdo do vetor
+print("\nPARSEA OS GRs\n\n")
+parsedGRs = parseGR(grs)                    # Parseia os GRs
+printFile(parsedGRs)                            # Imprime o conteúdo do vetor
+print("\nFIM DO TRATAMENTO DE GR")
+print(parsedGRs)
