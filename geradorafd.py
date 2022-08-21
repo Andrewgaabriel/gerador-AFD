b2aa867e320cd3359c6a67ad8f75c3e5d3fb246b
Andrew <andrew.gabrielgomes@gmail.com>
2022-08-21 17:31:23 -0300

    implementa a construção da AFND

--- geradorafd.py
+++ geradorafd.py
@@ -1,84 +1,68 @@
 import sys
-import re
+from this import d
+import pandas as pd
 
-""" -----------------Abrindo arquivo de log------------------------------------------------------- """
+
+
+ESTADOFINAL = 'D' # PROVISÓRIO ?
+
+
 def openFile(fileName):
     try:
         file = open(fileName, 'r')
         return file
     except:
         print('Erro ao abrir arquivo')
-""" -------------------------------------------------------------------------------------------- """
 
 
 
 
-""" -----------------------Função para imprimir conteúdo de um vetor/lista/arquivo--------------- """
 def printFile(file):
     for line in file:
         print(line)
 
-""" -------------------------------------------------------------------------------------------- """
 
 
 
-
-""" --------------------Função que pega o nome do arquivo de entrada------------------------------ """
 def getParam(arg):
     return sys.argv[arg]
 
-""" -------------------------------------------------------------------------------------------- """
 
 
-
-
-""" ----------------Pega os dados do arquivo e coloca em um vetor-------------------------------- """
 def getData(file):
     data = list()
     for line in file:
         data.append(line)
     return data
-""" -------------------------------------------------------------------------------------------- """
 
 
-
-""" -------------------Função que pega os tokens sujos------------------ """
 def getTokens(data):
     dirtyTokens = []
-
     for line in data:
         if line == '\n':
             break
         else:
             dirtyTokens.append(line)
     
-    
-
     return removeQuebraLinha(dirtyTokens)
-""" -------------------------------------------------------------------------------------------- """
 
-""" -------------------Função que pega os tokens sujos------------------ """
+
 def getGR(data):
     data.reverse()
     grs = []
-
     for line in data:
         if line == '\n':
             break
         else:
             grs.append(line)
     
-    
-
     return removeQuebraLinha(grs)
-""" -------------------------------------------------------------------------------------------- """
 
 
 
 
 def removeQuebraLinha(data):
     cleaned = []
-
     for token in data:
         cleaned.append(token.replace('\n', ''))
 
@@ -93,19 +77,15 @@ def parseTokens(tokens):
         for char in token:
             parsed.append(char)
     
-    #remove as duplicatas e ordena
     parsed = sorted(set(parsed))
-    #parsed = list(set(parsed))
 
     return parsed
 
 
 def parseGR(grs):
     parsedGRs = []
-
     
     for gr in grs:
-
         completedRule = []
 
         dirtyRuleName = gr.split('::=')[0]
@@ -113,6 +93,11 @@ def parseGR(grs):
 
         dirtyRules = gr.split('::=')[1]
         rules = dirtyRules.split('|')
+
+        for rule in rules:
+            rule = rule.replace(' ','s')
+
+        ruleName = ruleName.replace(' ','')
 
         completedRule.append(ruleName)
         completedRule.append(rules)
@@ -125,17 +110,16 @@ def parseGR(grs):
 def getGRStokens(gr):
     grTokens = []
 
-   
-
     for rule in gr:
         temp = rule.split('::=')[1]
         temp = temp.split('|')
+
         for aux in temp:
             aux = aux.split('<')
             grTokens.append(aux[0])
             
-    
     return sorted(set(grTokens))
+
 
 
 def removeBlankSpace(data):
@@ -145,6 +129,69 @@ def removeBlankSpace(data):
         cleaned.append(token.replace(' ', ''))
 
     return cleaned
+
+
+
+def createTable(tokens, grs):
+
+    indexes = []
+    for rule in grs:
+        indexes.append(rule[0])
+    indexes.append('*' + ESTADOFINAL)
+    
+    table = pd.DataFrame(index=indexes, columns=tokens)
+
+    return table
+
+
+
+def removeEpsilon(data):
+    cleaned = []
+
+    for token in data:
+        if token != 'ε':
+            cleaned.append(token)
+
+    return cleaned
+
+
+
+def fillWithGRs(table, grs):
+    for gr in grs:
+        ruleName = str(gr[0])   # nome da regra
+        rules = gr[1]           # regras em si
+
+        for rule in rules:
+            rule = rule.replace(' ', '').replace('\n', '')
+
+
+            if "ε" in rule: 
+                #IMPLEMENTAR TRATAMENTO DE QND OCORRER EPSILON
+                continue
+
+
+            elif rule.find('<') == -1: # SE TEM (<) RETORNA != -1
+                token = rule
+                table.loc[ruleName, token] += ',' + ESTADOFINAL
+
+            else: # SE TEM SIMBOLO NÃO-TERMINAL
+                rule = rule.replace('>', '')
+                ruleSplited = rule.split('<')
+                token = ruleSplited[0]
+                production = str(ruleSplited[1])
+                #print(token, production)
+                if table.loc[ruleName, token] == 'nan':
+                    table.loc[ruleName, token] = production
+                else:
+                    table.loc[ruleName, token] += ',' + production
+                
+        
+    return table
+
+
+def determiniza(afnd, tokens):
+    afd = pd.DataFrame(columns=tokens)
+    #implementar aqui a determinização da afnd
 
 
 
@@ -166,12 +213,37 @@ tokensFromGRS = getGRStokens(grs)                    # Pega os tokens do GRs
 #printFile(tokensFromGRS)                              # Imprime o conteúdo do vetor
 
 
-parsedTokens =removeBlankSpace(parsedTokens)
-tokensFromGRS =removeBlankSpace(tokensFromGRS)
+#parsedTokens =removeBlankSpace(parsedTokens)
+#tokensFromGRS =removeBlankSpace(tokensFromGRS)
 
-allTokens = parsedTokens + tokensFromGRS         # Merge dos tokens
+allTokens = removeBlankSpace(parsedTokens) + removeBlankSpace(tokensFromGRS)         # Merge dos tokens
 allTokens = sorted(set(allTokens))             # Remove as duplicatas e ordena
 #printFile(allTokens)                              # Imprime o conteúdo do vetor
 
 parsedGRs = parseGR(grs)                    # Parseia os GRs
 #printFile(parsedGRs)                            # Imprime o conteúdo do vetor
+
+
+allTokens = removeEpsilon(allTokens)
+
+
+afnd = createTable(allTokens, parsedGRs)
+print(afnd)
+afnd = afnd[allTokens].astype(str)
+afnd = fillWithGRs(afnd, parsedGRs)
+print(afnd)
+
+
+
+print(allTokens)
+print(parsedGRs)
+
+
+
+
+# TODO: IMPLEMENTAR A TABELA (CRIAR FUNÇÕES GENÉRICAS PARA FZR ESSAS TAREFAS)
+# TODO: TRANSFORMAR O AFND PARA O AFD
+# TODO: FAZER A MINIMIZAÇÃO DO AFD
+# TODO: GERAR O ARQUIVO DE SAIDA
+
+ 
