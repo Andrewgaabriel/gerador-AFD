65a6c562b8605ee87ae5bcb484f5fa6fe6cb23fd
Andrew <andrew.gabrielgomes@gmail.com>
2022-08-31 08:04:20 -0300

    feat: altera funcionamento de algumas funções + atualiza a entrada principal

--- geradorafd.py
+++ geradorafd.py
@@ -1,9 +1,9 @@
-from os import rename
 import sys
 import pandas as pd
 
 
 ESTADOFINAL = 'Z' # PROVISÓRIO ?
+possibleRules = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 allRules = []
 
 
@@ -15,13 +15,6 @@ def openFile(fileName):
         return file
     except:
         print('Erro ao abrir arquivo')
-
-
-
-
-def printFile(file):
-    for line in file:
-        print(line)
 
 
 
@@ -48,8 +41,7 @@ def getTokens(data):
             break
         else:
             dirtyTokens.append(line)
-            #parsedTokens = parseTokens(tokens)
-    return parseTokens(removeQuebraLinha(dirtyTokens))
+    return dirtyTokens
 
 
 
@@ -62,7 +54,6 @@ def getGR(data):
             break
         else:
             grs.append(line)
-            
     return removeQuebraLinha(grs)
 
 
@@ -148,14 +139,24 @@ def createTable(tokens, grs):
     indexes = []
     for rule in grs:
         indexes.append(rule[0])
-    indexes.append('*' + ESTADOFINAL) # adiciona estado final
+        allRules.append(rule[0])
+    #indexes.append('*' + ESTADOFINAL) # adiciona estado final
     indexes.append('ERR') # adiciona estado de erro
-    
     table = pd.DataFrame(index=indexes, columns=tokens)
+
+    
+
+    print('allrules:',allRules)
 
     return table
 
 
+def updatePossibleRules():
+    for rule in allRules:
+        if rule not in possibleRules:
+            continue
+        else:
+            possibleRules.remove(rule)
 
 def removeEpsilon(data):
     cleaned = []
@@ -165,28 +166,50 @@ def removeEpsilon(data):
             continue
         else:
             cleaned.append(token)
+    
 
     return cleaned
 
 
 
-def fillWithGRs(table, grs):
-    for gr in grs:
-        ruleName = str(gr[0])   # nome da regra
-        rules = gr[1]           # regras em si
+def virouTerminal(table, ruleName):
+
+    nova = '*' + ruleName
+    table = table.rename(index = {ruleName:nova})
+
+    return table
+
+
+
+
+def fillWithGRs(table, gramaticas):
+
+    print('Preenchendo tabela...', gramaticas)
+     #formato : [['A', [' e<A> ', ' i<A> ', '  ε']], ['S', [' e<A> ', ' i<A>']]]
+
+    for gr in gramaticas:
+        grName = str(gr[0])   # nome da gramatica : 'A'-> ['A', [' e<A> ', ' i<A> ', '  ε']]
+        rules = gr[1]           # regras : ' e<A> ', ' i<A> ', '  ε' -> ['A', [' e<A> ', ' i<A> ', '  ε']]
 
         for rule in rules:
+
             rule = rule.replace(' ', '').replace('\n', '')
 
+            if "ε" in rule or "Îµ" in rule: # Se tem epislon a regra em questão vira terminal
 
-            if "ε" in rule or "Îµ" in rule: 
-                #IMPLEMENTAR TRATAMENTO DE QND OCORRER EPSILON
+                print('Regra:', grName, 'tem epsilon')
+                table = virouTerminal(table, grName)
+
                 continue
 
 
-            elif rule.find('<') == -1: # SE TEM (<) RETORNA != -1
+            elif rule.find('<') == -1: # Se não tem (<) executa :: (É UM TERMINAL)
                 token = rule
-                table.loc[ruleName, token] += ',' + ESTADOFINAL
+                """ table = cria """ # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
+
+                table.loc[grName, token] += ',' + ESTADOFINAL
+                # ! inves de apontar para um estado final genérico será preciso criar um estado final especifíco para a situação
+
 
             else: # SE TEM SIMBOLO NÃO-TERMINAL
                 rule = rule.replace('>', '')
@@ -194,10 +217,10 @@ def fillWithGRs(table, grs):
                 token = ruleSplited[0]
                 production = str(ruleSplited[1])
                 #print(token, production)
-                if table.loc[ruleName, token] == 'nan':
-                    table.loc[ruleName, token] = production
+                if table.loc[grName, token] == 'nan':
+                    table.loc[grName, token] = production
                 else:
-                    table.loc[ruleName, token] += ',' + production
+                    table.loc[grName, token] += ',' + production
                 
         
     return ordenaTable(table)
@@ -281,8 +304,6 @@ def determiniza(afnd, tokens):
     for rule in newRules: # Percorre o vetor de novas regras
 
         dados = dict()
-        
-        #rule = '[' + rule.replace(',', '') + ']'
 
         toSearch = rule.split(',')
 
@@ -332,44 +353,6 @@ def cleanTable(table):
     return table
 
 
-""" def eliminaInalcancaveis(table):
-    terminais = [] # pega todos os terminais/regras
-    
-    for index in table.index:
-        aux = []
-        aux.append(index)
-        terminais.append(aux)
-    
-    for terminal in terminais:
-        for index in table.index:
-            if terminal[0] == index:
-                for token in table.columns:
-                    terminal.append(table.loc[index, token])
-            
-
-
-    return terminais
-
-def imprimeInalcançaveis(terminais):
-    for terminal in terminais:
-        for i in range(len(terminal)):
-            if i == 0:
-                print('Partida:', terminal[i])
-            else:
-                print(terminal[i])
-    print('\n')
-
-
-
-
-def eliminaMortos(table):
-    return table
-
-def minimizacao(afd):
-    afd = eliminaInalcancaveis(afd)
-    afd = eliminaMortos(afd)
-    return afd """
-
 
 def estadoDeErro(afd):
     """ ADICIONA O ESTADO DE ERRO """
@@ -383,23 +366,36 @@ def estadoDeErro(afd):
     return afd
 
 
+def carregaTokens(afnd, tokens):
+    print('Carregando tokens...')
+    return afnd
+
+
 allData = getData(openFile(getParam(1)))  # Pega os dados do arquivo de entrada e coloca em um vetor
 tokens = getTokens(allData)               # Pega os vetor gerado e coloca os tokens em um vetor
+parsedTokens = parseTokens(removeQuebraLinha(tokens))
+#print(tokens)   # TOKENS PARA GERAR A TABELA
 grs = getGR(allData)                      # Pega os vetor gerado e coloca as regras em um vetor
 tokensFromGRS = getGRStokens(grs)         # Pega os tokens presentes nas regras e coloca em um vetor
-allTokens = removeBlankSpace(tokens) + removeBlankSpace(tokensFromGRS) # Faz uma junção dos tokens (das regras e os fornecidos na entrada)
+allTokens = removeBlankSpace(parsedTokens) + removeBlankSpace(tokensFromGRS) # Faz uma junção dos tokens (das regras e os fornecidos na entrada)
 allTokens = sorted(set(allTokens))        # Remove as duplicatas e ordena
 parsedGRs = parseGR(grs)                  # Pega o vetor gerado e coloca as regras parseadas em um vetor
-allTokens = removeEpsilon(allTokens)      # Remove o epsilon das gramáticas
+allTokens = removeEpsilon(allTokens)      # Remove o epsilon dos tokens
+print('Tokens:', allTokens)              # Imprime os tokens
+
+
 
 
 afnd = createTable(allTokens, parsedGRs)    # cria a tabela (dataframe) para o AFND
+print('primeiros\n', afnd)
 afnd = afnd[allTokens].astype(str)          # altera o tipo de dados para string
 afnd = fillWithGRs(afnd, parsedGRs)         # preenche a tabela com as regras
+print(afnd)
 afnd = removeNan(afnd)                      # remove os 'nan'
 print(afnd)                                 # printa a tabela
 
-
+afnd = carregaTokens(afnd, tokens)          # função para carregar os tokens na afd
+print(afnd)
 
 afd= determiniza(afnd, allTokens)           # determiniza o afnd
 afd = afd[allTokens].astype(str)            # altera o tipo de dados para string
@@ -410,7 +406,7 @@ afd = ordenaTable(afd)                      # ordena a tabela e normaliza
 while temRegraNova(afd):                # função que verifica se há novas regras geradas a partir das regras já determinizadas
     afd = determiniza(afd, allTokens)   # determiniza o afnd
     afd = ordenaTable(afd)              # ordena a tabela e normaliza
-    print(afd)                          # printa a tabela a cada etapa
+    print( 'teste', afd)                          # printa a tabela a cada etapa
     print("\nAFD\n")
 
 
@@ -437,3 +433,6 @@ imprimeInalcançaveis(a) """
 
 # TODO: fazer minimização do AFD (i.e eliminar inalcançáveis e mortos)
 
+# PARA ALTERAR O NOME DO INDICE DA TABELA
+# df  =  df.rename(index = {'antigo':'novo'})
+print('allrules',allRules)
